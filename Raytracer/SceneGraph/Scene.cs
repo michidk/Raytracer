2d579a5c55375349ba0c1de94f9e73431b4ffa44
michidk <michidk@live.de>
2018-05-19 12:34:29 +0200

    fixed scene generation

--- Raytracer/SceneGraph/Scene.cs
+++ Raytracer/SceneGraph/Scene.cs
@@ -16,16 +16,13 @@ namespace Raytracer
 
         public static Scene BuildExampleScene()
         {
-            var viewPos = new Vector3D(0, 0, 0);
-            var viewDir = Vector3D.Forward;
-
-            var camera = new Camera();
-            camera.RecalculateMatrix(viewPos, viewPos + viewDir);
+            var camera = new Camera(new Vector3D(0, 0, -20), Vector3D.Forward);
 
             var scene = new Scene(camera);
 
-            scene.Renderables.Add(new Sphere(new Vector3D(0, 0, 0),
-                new Material(Material.MaterialType.Lambert, new Vector3D(1, 0, 0), new Vector3D(0, 0, 0), 0.2, 0), 1));
+            scene.Renderables.Add(new Sphere(new Vector3D(0, 0, 0), new Material(Material.MaterialType.Lambert, Color.FromSysColor(System.Drawing.Color.Aquamarine), new Color(0, 0, 0), 0.2, 0), 1));
+            scene.Renderables.Add(new Sphere(new Vector3D(1, 0, 0), new Material(Material.MaterialType.Lambert, Color.FromSysColor(System.Drawing.Color.Coral), new Color(0, 0, 0), 0.2, 0), 1));
+            scene.Renderables.Add(new Sphere(new Vector3D(-2, 0, 0), new Material(Material.MaterialType.Lambert, Color.FromSysColor(System.Drawing.Color.Chartreuse), Color.FromSysColor(System.Drawing.Color.Chartreuse), 0.2, 0), 4));
 
             return scene;
         }
